62fcbcaedb6e0c201872bbd06c02e6b5f3312d09
SwiftSeal <65286772+SwiftSeal@users.noreply.github.com>
2023-07-31 13:16:27 +0100

    pandas parsing

--- parse_output.py
+++ parse_output.py
@@ -1,17 +1,35 @@
 import sys
+import re
+import pandas as pd
 
-def process_input(input_data):
-    # Implement your logic to process the input data here
-    # For demonstration purposes, let's just print the input
-    print("Input received:", input_data)
+def clean_input(input_data):
+    # The nlrexpress long output is weirdly formatted.
+    # Some columns have | to delimit them.
+    # Some (not all) columns are separated by tabs.
+    # Whitespace is used to pad the columns.
+    # To fix this, Deleta all |, replace all whitespaces with a tab, then replace strings of tabs with a single tab.
+
+    input_data = input_data.replace("|", "")
+
+    # Replace all whitespace with a single tab
+    input_data = input_data.replace(" ", "\t")
+
+    # Replace all strings of tabs with a single tab
+    input_data = re.sub(r'\t+', '\t', input_data)
+
+    # create a pandas dataframe from the tabular data
+    df = pd.read_csv(input_data, sep="\t")
+
+    return df
 
 def main():
     try:
-        # Read input from stdin
+        # Get the raw tabular output of nlrexpress from stdin
         input_data = sys.stdin.read().strip()
 
-        # Process the input data
-        process_input(input_data)
+        df = clean_input(input_data)
+
+        print(df)
 
     except KeyboardInterrupt:
         # Handling KeyboardInterrupt (Ctrl+C) gracefully
